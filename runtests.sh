b6296446f1c42a6aae972a63de83aa03f132338a
Jonas Obrist <jonas.obrist@divio.ch>
2010-07-14 14:53:00 +0200

    added coverage.py to test runners

--- runtests.sh
+++ runtests.sh
@@ -3,14 +3,20 @@ cd tests
 if [ "`python -c 'import django; print django.get_version()' 2>/dev/null`" == "1.2.1" ]
   then
     cd testapp
-    python manage.py test testapp
-    retcode=$?
+    if which coverage &> /dev/null; then
+        coverage run manage.py test testapp
+        retcode=$?
+        coverage xml
+    else
+        python manage.py test testapp
+        retcode=$?
+    fi
     cd ..
   else
     echo "setting up test environment (this might take a while)..."
     python bootstrap.py >/dev/null 2>&1
     ./bin/buildout >/dev/null 2>&1
-    ./bin/test
+    ./bin/coverage testapp/manage.py test testapp
     retcode=$?
 fi
 cd ..
