9fcd5f6a0d7f83fe99028a9ec8025a1006bc2ea2
msaelices <msaelices@yaco.es>
2011-02-06 21:57:53 +0100

    Changed django-sekizai to use threading local to store contents added to every block. This is useful to get sekizai working in inclusion templatetags which does not transmit context directly to the child template (which is a usual thing). Storing in a variable local to the thread is much better than template context.

--- sekizai/context_processors.py
+++
@@ -1,9 +0,0 @@
-from sekizai.data import SekizaiDictionary
-from sekizai.settings import VARNAME
-
-def sekizai(request=None):
-    """
-    Simple context processor which makes sure that the SekizaiDictionary is
-    available in all templates.
-    """
-    return {VARNAME: SekizaiDictionary()}
