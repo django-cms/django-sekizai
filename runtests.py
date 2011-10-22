4332963f21ab45bce06d8aa3e5a76381686ebd42
Jonas Obrist <jonas.obrist@divio.ch>
2011-10-22 15:05:59 +0200

    Turned runtest.sh+buildout+magic into runtests.py+awesomeness

---
+++ runtests.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+urlpatterns = []
+
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'sqlite3',
+        'NAME': ':memory:'
+    }
+}
+
+
+INSTALLED_APPS = [
+    'sekizai',
+]
+
+TEMPLATE_DIRS = [
+    os.path.join(os.path.dirname(__file__), 'test_templates'),
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'sekizai.context_processors.sekizai',
+]
+    
+
+ROOT_URLCONF = 'runtests'
+
+def run_tests():
+    from django.conf import settings
+    settings.configure(
+        INSTALLED_APPS = INSTALLED_APPS,
+        ROOT_URLCONF = ROOT_URLCONF,
+        DATABASES = DATABASES,
+        TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner',
+        TEMPLATE_DIRS = TEMPLATE_DIRS,
+        TEMPLATE_CONTEXT_PROCESSORS = TEMPLATE_CONTEXT_PROCESSORS,
+        TEMPLATE_DEBUG = TEMPLATE_DEBUG
+    )
+
+    # Run the test suite, including the extra validation tests.
+    from django.test.utils import get_runner
+    TestRunner = get_runner(settings)
+
+    test_runner = TestRunner(verbosity=1, interactive=False, failfast=False)
+    failures = test_runner.run_tests(INSTALLED_APPS)
+    return failures
+
+
+if __name__ == "__main__":
+    failures = run_tests()
+    if failures:
+        sys.exit(bool(failures))
