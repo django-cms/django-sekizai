3e9b04d5064e11e07f496da5c4f4bb63275b5b5e
Jonas Obrist <ojiidotch@gmail.com>
2013-01-18 15:47:38 +0900

    added classifiers to setup.py (Python 3 support!)

--- setup.py
+++ setup.py
@@ -16,4 +16,17 @@ setup(
         'django-classy-tags>=0.3.1',
     ],
     test_suite = 'runtests.main',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+    ]
 )
