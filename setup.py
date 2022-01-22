b147cf4b306d5727582f5cd5109f86b28f9f76b4
Mark Walker <mark.walker@realbuzz.com>
2022-01-22 00:19:25 +0000

    Drop django 3.0 support due to security issues

--- setup.py
+++ setup.py
@@ -23,7 +23,6 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.10',
     'Framework :: Django',
     'Framework :: Django :: 2.2',
-    'Framework :: Django :: 3.0',
     'Framework :: Django :: 3.1',
     'Framework :: Django :: 3.2',
     'Topic :: Internet :: WWW/HTTP',
