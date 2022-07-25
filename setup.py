eafee144b1000ebce629011c9eb1606d92bb41b0
Mark Walker <mark.walker@realbuzz.com>
2022-07-26 00:38:02 +0100

    Adding django 4.1 to tests

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ CLASSIFIERS = [
     'Framework :: Django',
     'Framework :: Django :: 3.2',
     'Framework :: Django :: 4.0',
+    'Framework :: Django :: 4.1',
     'Topic :: Internet :: WWW/HTTP',
     'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     'Topic :: Software Development',
