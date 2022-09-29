dc5ceddb74c5de37a530c0450979b9cc569c8e96
Fabian Braun <fsbraun@gmx.de>
2022-09-29 22:48:03 +0200

    Remove debug statements

--- sekizai/helpers.py
+++ sekizai/helpers.py
@@ -89,9 +89,8 @@ def _extend_nodelist(extend_node):
 def _scan_namespaces(nodelist, current_block=None):
     from sekizai.templatetags.sekizai_tags import RenderBlock, WithData
     found = []
-    print("SCAN")
+
     for node in nodelist:
-        print(node, type(node))
         # check if this is RenderBlock node
         if isinstance(node, (RenderBlock, WithData)):
             # resolve it's name against a dummy context
