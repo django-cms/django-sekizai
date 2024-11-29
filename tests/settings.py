6784f7e343ce7124e755378e7797958b914ba8db
Fabian Braun <fsbraun@gmx.de>
2024-11-29 22:04:13 +0100

    Update tests/settings.py

--- tests/settings.py
+++ tests/settings.py
@@ -60,6 +60,6 @@ def run():
 
 if __name__ == '__main__':
     import sys
-     # Add current directory to python path - works if this script is called python tests/settings,.py
+    # Add current directory to python path - works if this script is called python tests/settings,.py
     sys.path.insert(0, '.')
     run()
